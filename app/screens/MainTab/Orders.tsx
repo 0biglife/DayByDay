fbe6dfd581d2892c60d05f5b8e9a2fb9910d82d2
0biglife <kimchoon42@gmail.com>
2022-03-22 19:56:55 +0900

    OrderView - FlatList

--- app/screens/MainTab/Orders.tsx
+++ app/screens/MainTab/Orders.tsx
@@ -1,7 +1,8 @@
 import React, {useCallback} from 'react';
-import {Alert, ScrollView} from 'react-native';
+import {Alert, FlatList, ScrollView} from 'react-native';
 import {useSelector} from 'react-redux';
 import styled from 'styled-components/native';
+import {Order} from '../../redux/slices/order';
 import {RootState} from '../../redux/store/reducers';
 
 const Container = styled.View`
@@ -27,16 +28,25 @@ const Orders = () => {
     Alert.alert('test');
   }, []);
 
+  const renderItem = useCallback(({item}: {item: Order}) => {
+    return (
+      <Container>
+        <CellContainer onPress={toggleDetail}>
+          <Title style={{fontSize: 20, color: 'black'}}>
+            {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+          </Title>
+        </CellContainer>
+      </Container>
+    );
+  }, []);
+
   return (
-    <ScrollView>
-      {orders.map(v => (
-        <Container>
-          <CellContainer onPress={toggleDetail}>
-            <Title style={{fontSize: 20, color: 'black'}}>{v.orderId}</Title>
-          </CellContainer>
-        </Container>
-      ))}
-    </ScrollView>
+    //반복문 or 서버 데이터 받을 떄는 ScrollView 보단 FlatList 를 지향
+    <FlatList
+      data={orders}
+      keyExtractor={item => item.orderId}
+      renderItem={renderItem}
+    />
   );
 };
 
