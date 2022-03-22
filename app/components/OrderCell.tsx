a6e91eff4a27fc02c692409921e3c8e71b68877c
0biglife <kimchoon42@gmail.com>
2022-03-22 21:05:20 +0900

    OrderView API & Navigation

--- app/components/OrderCell.tsx
+++ app/components/OrderCell.tsx
@@ -1,7 +1,15 @@
+import axios, {AxiosError} from 'axios';
 import React, {useCallback, useState} from 'react';
-import {View} from 'react-native';
+import {Alert, View} from 'react-native';
+import {useSelector} from 'react-redux';
 import styled from 'styled-components/native';
-import {Order} from '../redux/slices/order';
+import orderSlice, {Order} from '../redux/slices/order';
+import {useAppDispatch} from '../redux/store';
+import {RootState} from '../redux/store/reducers';
+import {Config} from 'react-native-config';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {MainTabParamList} from '../navigations/Types';
 
 const Container = styled.View`
   //
@@ -39,13 +47,21 @@ const DetailView = styled.View`
 `;
 
 const ButtonWrapper = styled.View`
+  background-color: lightgray;
+  border-radius: 10px;
+  height: 40px;
   flex-direction: row;
-  background-color: lightblue;
+  margin-top: 10px;
+  justify-content: space-between;
   justify-content: center;
   align-items: center;
 `;
 
-const TouchButton = styled.TouchableOpacity``;
+const TouchButton = styled.TouchableOpacity`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
 
 const ButtonText = styled.Text`
   font-size: 16px;
@@ -53,10 +69,43 @@ const ButtonText = styled.Text`
 `;
 
 const OrderCell = ({item}: {item: Order}) => {
+  //상단 컴포넌트에서 navigation을 인자로 줄 수 있지만 그런 props-dealing은 지양하는게 성능상 좋음(추후 자식 컴포넌트가 생길 것을 고려하여)
+  const navigation =
+    useNavigation<NativeStackNavigationProp<MainTabParamList>>();
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState<boolean>(false);
   const [detail, setDetail] = useState<boolean>(false);
+  const accessToken = useSelector((state: RootState) => state.user.accessToken);
   const toggleDetail = useCallback(() => {
     setDetail(prev => !prev);
   }, []);
+
+  const onAccept = useCallback(async () => {
+    try {
+      setLoading(true);
+      await axios.post(
+        `${Config.API_URL}/accept`,
+        {orderId: item.orderId},
+        {headers: {Authorization: `Beare ${accessToken}`}},
+      );
+      dispatch(orderSlice.actions.acceptOrder(item.orderId));
+      navigation.navigate('Delivery');
+    } catch (error) {
+      const errorResponse = (error as AxiosError).response;
+      //이미 누가 주문을 잡아간 상태면 백에서 400 에러코드를 보내주고 다시 reject 해준다
+      if (errorResponse?.status === 400) {
+        Alert.alert(errorResponse.data.message);
+        dispatch(orderSlice.actions.rejectOrder(item.orderId));
+      }
+    } finally {
+      setLoading(false);
+    }
+    dispatch(orderSlice.actions.acceptOrder(item.orderId));
+  }, [accessToken, dispatch, item.orderId, navigation]);
+
+  const onReject = useCallback(() => {
+    dispatch(orderSlice.actions.rejectOrder(item.orderId));
+  }, [dispatch, item.orderId]);
 
   return (
     <Container key={item.orderId}>
@@ -80,10 +129,10 @@ const OrderCell = ({item}: {item: Order}) => {
             <Title>데이터 공간</Title>
           </View>
           <ButtonWrapper>
-            <TouchButton>
+            <TouchButton onPress={onAccept} disabled={loading}>
               <ButtonText>Accept</ButtonText>
             </TouchButton>
-            <TouchButton>
+            <TouchButton onPress={onReject} disabled={loading}>
               <ButtonText>Reject</ButtonText>
             </TouchButton>
           </ButtonWrapper>
